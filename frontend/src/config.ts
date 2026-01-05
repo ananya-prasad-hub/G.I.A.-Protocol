74ba842fb3b852e398aeb19fd19a595633cb201d
Shantanu Gautam <gshantanu475@gmail.com>
2026-01-05 19:08:34 +0530

    Change API_BASE to new Railway URL
    Updated API_BASE to point to the new Railway URL.

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE =
-  "https://railway.com/project/e4849c2a-d7ff-43a8-8094-222da1f7466f?environmentId=93af0ccb-f305-4935-bc5f-105ab2902fe7"; // replace with your Railway URL
+  "gia-protocol-production.up.railway.app"; // replace with your Railway URL
